1687543a25d3e6442a6c8213e29386f6464fbb99
alexei.svitkine <alexei.svitkine@d0c427fb-afb2-0230-4e54-e18847941633>
2010-04-11 01:31:16 +0000

    get info on files

--- HotWiredBridge/src/hotwiredbridge/hotline/Transaction.java
+++ HotWiredBridge/src/hotwiredbridge/hotline/Transaction.java
@@ -31,6 +31,7 @@ public class Transaction {
 	public static final int ID_AGREE = 121;
 
 	public static final int ID_GETFOLDERLIST = 200;
+	public static final int ID_GETFILEINFO = 206;
 
 	public static final int ID_GETUSERLIST = 300;
 	public static final int ID_USERCHANGE = 301;
