a103b4f178bf9f6922a35a0f23a58f70a238f4ba
alexei.svitkine <alexei.svitkine@d0c427fb-afb2-0230-4e54-e18847941633>
2011-03-13 23:21:52 +0000

    fixing flag values

--- HotWiredBridge/src/hotwiredbridge/hotline/HotlinePrivileges.java
+++ HotWiredBridge/src/hotwiredbridge/hotline/HotlinePrivileges.java
@@ -3,33 +3,37 @@ package hotwiredbridge.hotline;
 import java.io.*;
 
 public class HotlinePrivileges {
-	public static final int CAN_GET_USER_INFO = 31;
-	public static final int CAN_UPLOAD_ANYWHERE = 30;
-	public static final int CAN_USE_ANY_NAME = 29;
-	public static final int DONT_SHOW_AGREEMENT = 28;
-	public static final int CAN_COMMENT_FILES = 27;
-	public static final int CAN_COMMENT_FOLDERS = 26;
-	public static final int CAN_VIEW_DROP_BOXES = 25;
-	public static final int CAN_MAKE_ALIASES = 24;
-	public static final int CAN_READ_USERS = 23;
-	public static final int CAN_MODIFY_USERS = 22;
-	public static final int CAN_READ_NEWS = 19;
-	public static final int CAN_POST_NEWS = 18;
-	public static final int CAN_DISCONNECT_USERS = 17;
-	public static final int CANNOT_BE_DISCONNECTED = 16;
-	public static final int CAN_MOVE_FOLDERS = 15;
-	public static final int CAN_READ_CHAT = 14;
-	public static final int CAN_SEND_CHAT = 13;
-	public static final int CAN_CREATE_USERS = 9;
-	public static final int CAN_DELETE_USERS = 8;
-	public static final int CAN_DELETE_FILES = 7;
-	public static final int CAN_UPLOAD_FILES = 6;
-	public static final int CAN_DOWNLOAD_FILES = 5;
-	public static final int CAN_RENAME_FILES = 4;
-	public static final int CAN_MOVE_FILES = 3;
-	public static final int CAN_CREATE_FOLDERS = 2;
-	public static final int CAN_DELETE_FOLDERS = 1;
-	public static final int CAN_RENAME_FOLDERS = 0;
+
+
+	public static final int CAN_DELETE_FILES = 63;
+	public static final int CAN_UPLOAD_FILES = 62;
+	public static final int CAN_DOWNLOAD_FILES = 61;
+	public static final int CAN_RENAME_FILES = 60;
+	public static final int CAN_MOVE_FILES = 59;
+	public static final int CAN_CREATE_FOLDERS = 58;
+	public static final int CAN_DELETE_FOLDERS = 57;
+	public static final int CAN_RENAME_FOLDERS = 56;
+	public static final int CAN_MOVE_FOLDERS = 55;
+	public static final int CAN_READ_CHAT = 54;
+	public static final int CAN_SEND_CHAT = 53;
+	public static final int CAN_START_CHATS = 52;
+	public static final int CAN_CREATE_USERS = 49;
+	public static final int CAN_DELETE_USERS = 48;
+	public static final int CAN_READ_USERS = 47;
+	public static final int CAN_MODIFY_USERS = 46;
+	public static final int CAN_READ_NEWS = 43;
+	public static final int CAN_POST_NEWS = 42;
+	public static final int CAN_DISCONNECT_USERS = 41;
+	public static final int CANNOT_BE_DISCONNECTED = 40;
+	public static final int CAN_GET_USER_INFO = 39;
+	public static final int CAN_UPLOAD_ANYWHERE = 38;
+	public static final int CAN_USE_ANY_NAME = 37;
+	public static final int DONT_SHOW_AGREEMENT = 36;
+	public static final int CAN_COMMENT_FILES = 35;
+	public static final int CAN_COMMENT_FOLDERS = 34;
+	public static final int CAN_VIEW_DROP_BOXES = 33;
+	public static final int CAN_MAKE_ALIASES = 32;
+
 
 	private long value;
 
@@ -44,18 +48,22 @@ public class HotlinePrivileges {
 		}
 	}
 
-	public boolean hasPrivilege(int bit) {
-		return (value & (1L << (63-bit))) != 0;
+	private long flagForBit(int bit) {
+		return (1L << bit);
 	}
 	
+	public boolean hasPrivilege(int bit) {
+		return (value & flagForBit(bit)) != 0;
+	}
+
 	public void setPrivilege(int bit, boolean bitValue) {
 		if (bitValue) {
-			value |= (1L << (63-bit));
+			value |= flagForBit(bit);
 		} else {
-			value &= ~(1L << (63-bit));
+			value &= ~flagForBit(bit);
 		}
 	}
-	
+
 	public byte[] toByteArray() {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		DataOutputStream dout = new DataOutputStream(out);
