e2ff08301ca8fddd2722aa81f02583175694a3d6
alexei.svitkine <alexei.svitkine@d0c427fb-afb2-0230-4e54-e18847941633>
2011-11-13 20:15:11 +0000

    ini file properties

---
+++ HotWiredBridge/src/hotwiredbridge/ServerConfigReader.java
@@ -0,0 +1,26 @@
+package hotwiredbridge;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ServerConfigReader {
+	public static ServerConfig read(String path) throws IOException {
+		ServerConfig config = new ServerConfig();
+		Properties properties = new Properties();
+		FileInputStream in = new FileInputStream(path);
+		try {
+			properties.load(in);
+			config.setWiredHost(properties.getProperty("WIRED_HOST"));
+			config.setWiredPort(Integer.valueOf(properties.getProperty("WIRED_PORT")));
+			config.setIconsFilePath(properties.getProperty("ICON_FILE_PATH"));
+			config.setAppleVolumesFilePath(properties.getProperty("APPLE_VOLUMES_FILE_PATH"));
+			config.setCertificatesFilePath(properties.getProperty("CERTIFICATES_FILE_PATH"));
+			config.setCertificatesFilePassphrase(properties.getProperty("CERTIFICATES_FILE_PASSPHRASE"));
+			config.setHotlinePort(Integer.valueOf(properties.getProperty("HOTLINE_PORT")));
+		} finally {
+			in.close();
+		}
+		return config;
+	}
+}
