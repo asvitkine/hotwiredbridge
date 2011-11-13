a0696a881689fa730e8295f48f2e9eb54db42a52
alexei.svitkine <alexei.svitkine@d0c427fb-afb2-0230-4e54-e18847941633>
2011-11-13 19:46:47 +0000

    add wired files into the same jar

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 cd HotWiredBridge/bin
-FILES=`find hotwiredbridge org -name '*.class' -print`
+rm -rf tmp
+mkdir tmp
+cd tmp
+unzip ../../lib/wired.jar
+cd ..
+mv tmp/wired .
+FILES=`find hotwiredbridge org wired -name '*.class' -print`
 TARGET=../../hotwired.jar
 rm "$TARGET"
 jar cvfm "$TARGET" ../../hotwired.manifest $FILES
+rm -rf tmp wired
 
